a41a93576b7c0b0e671ea5aa28a72852b9adb95d
Karlie-crypto <karlieemoyo@gmail.com>
2022-09-08 16:42:53 -0700

    Unix is basically

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *main - Entry point
  *Return: Always 0 (Success)
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,24 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
-return (0);
+#include <stdio.h>                                                                                                                      
+                                                                                                                                        
+/**                                                                                                                                     
+* main - print the string in the put function                                                                                           
+*                                                                                                                                       
+* Description: using the main function                                                                                                  
+* this program prints "Programming is like building a multilingual puzzle                                                               
+* Return: 0                                                                                                                             
+*/                                                                                                                                      
+int main(void)                                                                                                                          
+{                                                                                                                                       
+        char c;                                                                                                                         
+        int i;                                                                                                                          
+        long li;                                                                                                                        
+        long long lli;                                                                                                                  
+        float f;                                                                                                                        
+                                                                                                                                        
+        printf("Size of a char: %ld byte(s)\n", sizeof(c));                                                                             
+        printf("Size of an int: %ld byte(s)\n", sizeof(i));                                                                             
+        printf("Size of a long int: %ld byte(s)\n", sizeof(li));                                                                        
+        printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));                                                                  
+        printf("Size of a float: %ld byte(s)\n", sizeof(f));                                                                            
+        return (0);                                                                                                                     
 }
