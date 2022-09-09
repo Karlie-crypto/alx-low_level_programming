730f4ebeb613a33f7c3c3365c2a35667815b12af
Karlie-crypto <karlieemoyo@gmail.com>
2022-09-09 15:29:48 -0700

    Seven print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include <ctype.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * positive,negative or zero
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+{
