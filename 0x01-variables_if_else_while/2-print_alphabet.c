de6517b1fdabc42cb3391ead10dd9b5cc275e794
Karlie-crypto <karlieemoyo@gmail.com>
2022-09-09 15:18:26 -0700

    Two alphabet print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,16 +9,12 @@
  */
 int main(void)
 {
-	int letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		if (letter == 'q' || letter == 'e')
-		{
-			continue;
-		}
-		putchar(letter);
-	}
-	putchar('\n');
-	return (0);
+int alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+alphabet = tolower(alphabet);
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
 }
