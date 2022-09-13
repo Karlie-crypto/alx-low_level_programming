7aeccea4ca719928d0b0b12eea3b5de70ab49643
Karlie-crypto <karlieemoyo@gmail.com>
2022-09-13 11:35:35 -0700

    Four isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,42 +1,24 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - finds and prints the first 98 Fibonacci numbers,
- * starting with 1 and 2
- * followed by a new line
- * Return: ALways 0 (Success)
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ *
+ * @c: The int to print
+ * Return: Always 0.
  */
-int main(void)
+
+int _isalpha(int c)
+
 {
-	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	j = 1;
-	k = 2;
-
-	printf("%lu", j);
-
-	for (i = 1; i < 91; i++)
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
 	{
-		printf(", %lu", k);
-		k = k + j;
-		j = k - j;
+		return (1);
 	}
 
-	j1 = j / 1000000000;
-	j2 = j % 1000000000;
-	k1 = k / 1000000000;
-	k2 = k % 1000000000;
-
-	for (i = 92; i < 99; ++i)
+	else
 	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
-		k1 = k1 + j1;
-		j1 = k1 - j1;
-		k2 = k2 + j2;
-		j2 = k2 - j2;
+		return (0);
 	}
 
-	printf("\n");
-
-	return (0);
 }
