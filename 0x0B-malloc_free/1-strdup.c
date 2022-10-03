5b4d92ff81939e8413bbb7761511273a570f85ca
Karlie-crypto <karlieemoyo@gmail.com>
2022-10-03 02:58:25 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - Main Entry
+ * @str: input
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *nstr;
+	unsigned int len, i;
+
+	/* check is str is null */
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	nstr = malloc(sizeof(char) * (len + 1));
+
+	/*check if malloc was successful*/
+	if (nstr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		nstr[i] = str[i];
+	}
+	nstr[len] = '\0';
+	return (nstr);
+}
