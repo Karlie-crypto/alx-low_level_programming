d0e0bdbcc8793fd74ee3c215240a6a4015bb9d07
Karlie-crypto <karlieemoyo@gmail.com>
2022-10-06 01:20:21 -0700

    More malloc, free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Main Entry
+ * @nmemb: input
+ * @size: input
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+	return (p);
+}
