a0ccd91612849d44c0503c78ea38547a3b2c66b6
Karlie-crypto <karlieemoyo@gmail.com>
2022-10-10 05:27:08 -0700

    Structures, typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function to free memory
+ * @d: is pointer to dog_t
+**/
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
