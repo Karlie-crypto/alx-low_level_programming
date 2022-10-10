7e1090acadef56fbed3c58fce87b5131ea514353
Karlie-crypto <karlieemoyo@gmail.com>
2022-10-10 06:38:18 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * File: O-object_like_macro
+ *
+ * Desc: haeder file
+ */
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File: main
+ *
+ * Desc: header file
+ */
+int _putchar(char c);
+
+#endif
