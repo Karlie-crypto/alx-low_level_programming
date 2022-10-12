9462e0e8dd7522c2069081e8dd000583709e3833
Karlie-crypto <karlieemoyo@gmail.com>
2022-10-12 04:06:45 -0700

    Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include "functions_pointers.h"
+#include "function_pointers.h"
 
 /**
- * print_name - prints a name as is 
+ * print_name - prints a name as is
  * @name: name of the person
- *
+ * @f: is pointer
  * Return: Nothing.
- **/
-void print_name_as_is(char *name, void (*f)(char *))
+**/
+void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
 {
