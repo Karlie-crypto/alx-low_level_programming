d7ee6da03037bc4ac5209dc7328fe65c6618e9f5
Karlie-crypto <karlieemoyo@gmail.com>
2022-12-12 06:58:35 -0800

    Dynamic libraries

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,5 +24,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+extern div_t div(int __numer, int __denom);
 
-#endif
+#endif /* MAIN_H */
