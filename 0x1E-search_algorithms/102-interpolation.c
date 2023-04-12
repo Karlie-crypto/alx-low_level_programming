48a8b6f2340dc5a9c301bf20e74db7af2ca8d694
karlie-crypto <karlieemoyo@gmail.com>
2023-04-11 19:42:42 -0700

    A function that searches for a value in a sorted array of integers using the Interpolation search algorithm

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_seacrch - seaches for a value in a sorted array of integers
+ * using the interpolation search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in the array
+ * @value: the value to search for
+ * Return: first index where value is located
+ **/
+int interpolation_search(int *array, size_t size, int value);
+{
+	size_t i, l, r;
+
+	if (array == NULL)
+		return (-1);
+	for (l = 0, r = size - 1; r >= l;)
+	{
+		i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
+		if (i < size)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n", i);
+			break;
+		}
+
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			r = i - 1;
+		else
+			l = i + 1;
+	}
+
+	return (-1);
+}
